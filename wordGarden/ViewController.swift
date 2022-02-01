51ede08990be11d7b2922beabeff228019c31c52
Brennan Twiggs <btwiggs@zagmail.gonzaga.edu>
2022-01-31 17:11:36 -0800

    Completed 3.3

--- wordGarden/ViewController.swift
+++ wordGarden/ViewController.swift
@@ -25,10 +25,30 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let text = guessLetterField.text!
+        guessLetterButton.isEnabled = !(text.isEmpty)
+
+    }
+    func updateUIAfterGuess() {
+        guessLetterField.resignFirstResponder()
+        guessLetterField.text! = ""
+        guessLetterButton.isEnabled = false
     }
 
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        let text = guessLetterField.text!
+        guessLetterButton.isEnabled = !(text.isEmpty)
+    }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        //this dismisses the keyboard
+        updateUIAfterGuess()
+    }
+    
+
     @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+        //this dismisses the keyboard
+        updateUIAfterGuess()
     }
     
     @IBAction func playAgainButtonPressed(_ sender: UIButton) {
